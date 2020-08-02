be67f3fcd163c72825c03f77a32dd253ea77a1ad
dasagreeva <akashraj5399@gmail.com>
2020-08-02 13:36:55 +0530

    experkejrlk

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse, PlainTextResponse
+from fastapi.exceptions import RequestValidationError
 
 app = FastAPI()
 
+items = {"foo": "thinker of the fighter of the decoder"}
+
+
+class UnicornException(Exception):
+    def __init__(self, name: str):
+        self.name = name
+
+
+@app.exception_handler(RequestValidationError)
+async def unicorn_exception(request: Request, exc : UnicornException):
+    return PlainTextResponse(str(exc), status_code=400)
+
 
 @app.get("/items/{item_id}")
-async def read_items(
-        *, item_id :int = Path(..., title="The ID of the item to get")  ,q: str
-):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
-    return results
+async def read_item(item_id: int):
+    if item_id not in items:
+        pass
+    return {"theBomb": "Bomber Booty"}
